85241947d0f49aabb8a3617fc7d17cb05a33cf71
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-07-22 09:25:49 +0530

    🚀 RELEASE: Dart basic

---
+++ playground.dart
@@ -0,0 +1,22 @@
+// import 'dart:core';
+import 'dart:io';
+
+main() {
+  // var firstName = 'Sumesh';
+  // String lastName = 'Majhi';
+
+  // print(firstName + ' ' + lastName);
+
+  stdout.writeln('What is your name: ?');
+  String name = stdin.readLineSync();
+  print('My name is $name');
+}
+
+// In-line comment
+
+/* 
+Block comment
+multiple line of comment
+ */
+
+/// Documentation
