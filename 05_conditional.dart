13179452837fbf551e24d1cb217f23ab53ac5c46
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-08-01 08:54:00 +0530

    📦 NEW: Add looping

---
+++ 05_conditional.dart
@@ -0,0 +1,26 @@
+// Conditional Statement
+
+void main() {
+  // int number = 100;
+
+  // if (number % 2 == 0) {
+  //   print('Even');
+  // } else if (number % 3 == 0) {
+  //   print('Odd');
+  // } else {
+  //   print('Confused');
+  // }
+
+  int number = 0;
+
+  switch (number) {
+    case 0:
+      print('Even');
+      break;
+    case 1:
+      print('Odd');
+      break;
+    default:
+      print('Confused');
+  }
+}
